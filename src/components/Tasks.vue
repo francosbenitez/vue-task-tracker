c15ef6e45a8dc9261dbe056b3bda37120d9f5452
francosbenitez <francosbenitez@gmail.com>
2021-10-25 19:42:41 -0300

    feat: added tasks

---
+++ src/components/Tasks.vue
@@ -0,0 +1,14 @@
+<template>
+    <div v-for="(task, i) in tasks" :key="i">
+        {{ task.text }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Tasks',
+    props: {
+        tasks: Array
+    }
+}
+</script>
