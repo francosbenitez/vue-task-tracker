75316aaa728c9a5a50264451252ed095152df837
francosbenitez <francosbenitez@gmail.com>
2021-10-26 08:36:12 -0300

    feat: implemented AddTask

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <div class="container">
       <Header title="Task Tracker" />
+      <AddTask @add-task="addTask" />
       <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
     </div>
   </div>
@@ -10,12 +11,14 @@
 <script>
 import Header from '@/components/Header.vue'
 import Tasks from '@/components/Tasks.vue'
+import AddTask from '@/components/AddTask.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Tasks
+    Tasks,
+    AddTask
   },
   data () {
     return {
@@ -23,6 +26,9 @@ export default {
     }
   },
   methods: {
+    addTask (task) {
+      this.tasks = [...this.tasks, task]
+    },
     deleteTask (id) {
       if (confirm('Are you sure?')) {
         this.tasks = this.tasks.filter((task) => task.id !== id)
